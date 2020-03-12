ea47ca1ba2cc53110f32829f1fd65155d503386d
C2C Dev 005 <c2cdev005@Cam-iMac.local>
2020-03-12 13:34:00 +0700

    final commit

---
+++ src/RandomQuoteMachine.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import { genarateQuote } from './actions';
+import quotes from './resources/quotes';
+import './style.css';
+
+const colors = ['rgb(155, 89, 182)', 'rgb(52, 34, 36)', 'rgb(44, 62, 80)', 'rgb(22, 160, 133)', 'rgb(231, 76, 60)', 'rgb(39, 174, 96)']
+
+class RandomQuoteMachine extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: ''
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      color: this.getRandomColor()
+    })
+    this.props.dispatch(genarateQuote(this.getRandomQuote()))
+  }
+
+  handleChange = () => {
+    this.setState({
+      color: this.getRandomColor()
+    })
+    this.props.dispatch(genarateQuote(this.getRandomQuote()))
+  }
+
+  getRandomQuote() {
+    return quotes[Math.floor(Math.random() * quotes.length)]
+  }
+
+  getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+  }
+
+  render() {
+      const color = {
+        'color': this.state.color,
+        'backgroundColor': this.state.color,
+        'opacity': 0.8,
+        'transition': '1s'
+      }
+      const buttonColor = {
+        'backgroundColor': this.state.color,
+        'opacity': 0.8,
+        'transition': '1s'
+      }
+    return (
+      <div className="wrapper" style={color}>
+        <div id="quote-box">
+          <p id="text">{this.props.quote.content}</p>
+          <p id="author">- {this.props.quote.author}</p>
+          <div className="container">
+            <a id='tweet-quote' href='twitter.com/intent/tweet' style={buttonColor}>Tweet</a>
+            <button id="new-quote" onClick={this.handleChange} style={buttonColor}>New Quote</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    quote: state.quoteGenarate
+  }
+}
+
+export default connect(mapStateToProps)(RandomQuoteMachine);
---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export const NEW_QUOTE = 'NEW_QUOTE';
+
+export const genarateQuote = (quote) => ({
+    type: NEW_QUOTE,
+    quote
+})
